d26dbf5552ac1393b206d41bb9c87c3faa274bcd
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2023-05-04 22:59:53 +0530

    Create patterns25.cpp

---
+++ Patterns.cpp/patterns25.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int row = 1;
+    while(row<=n){
+
+        //Printing Space
+        int space = row - 1;
+        while(space){
+            cout << " ";
+            space = space - 1;
+        }
+
+        //printing Stars
+        int col = 1;
+        while(col <= n - row + 1){ 
+            int num = col + row - 1;
+            cout << num;
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
+}
